52d473631ce3faefb6907168c8d48a2c32dea266
jojaehyun <jojae38@naver.com>
2026-01-30 18:14:37 +0900

    중간 저장

--- 2.firmware/Comp/RFID.c
+++ 2.firmware/Comp/RFID.c
@@ -6,6 +6,7 @@
  */
 
 #include "RFID.h"
+#include "cli.h"
 
 #define DEFAULT_RFID_TIMEOUT 500
 
@@ -18,6 +19,7 @@ RFID_tbl_t RFID_tbl = {
     .isinit = false,
 };
 
+uint8_t st25_cmd_dr(uint8_t addr) { return (uint8_t)(0xC0 | (addr & 0x3F)); }
 uint8_t st25_cmd_wr(uint8_t addr) { return (uint8_t)(0x00 | (addr & 0x3F)); }
 uint8_t st25_cmd_rd(uint8_t addr) { return (uint8_t)(0x40 | (addr & 0x3F)); }
 
@@ -44,6 +46,20 @@ HAL_StatusTypeDef rfidSpiTransmit(uint8_t address, uint8_t* pdata, uint8_t len)
   return status;
 }
 
+HAL_StatusTypeDef rfidSpiDrTransmit(uint8_t address, uint8_t len)
+{
+  if(len == 0) return HAL_ERROR;
+
+  BSS_L();
+  HAL_StatusTypeDef status = HAL_OK;
+  uint8_t dr_addr = st25_cmd_dr(address);
+
+  status = HAL_SPI_Transmit(RFID_tbl.SPI_HANDLER, &dr_addr, len, DEFAULT_RFID_TIMEOUT);
+  BSS_H();
+
+  return status;
+}
+
 
 HAL_StatusTypeDef rfidSpiReceive(uint8_t address, uint8_t* pdata, uint8_t len)
 {
@@ -63,41 +79,74 @@ HAL_StatusTypeDef rfidSpiReceive(uint8_t address, uint8_t* pdata, uint8_t len)
 void rfidInit(void)
 {
   uint8_t buffer[20];
+  uint8_t data = 0;
   memset(buffer,0,20);
   RFID_tbl.isinit = true;
+
 
 //ID 확인 - 통신 확인
   if(rfidSpiReceive(IC_IDENTITY, buffer, 1) == HAL_OK)
   {
-    if(buffer[0] != 0x31) goto error;
+    if(buffer[0] != ST25R3916B_IC_IDENTITY) goto error;
   }
   else goto error;
 
 //설정 변경
+  data = IO_CONFIGURATION_REG_01_DATA;
+  if(rfidSpiTransmit(IO_CONFIGURATION_REG_01, &data, 1) != HAL_OK) goto error;
 
+  data = IO_CONFIGURATION_REG_02_DATA;
+  if(rfidSpiTransmit(IO_CONFIGURATION_REG_02, &data, 1) != HAL_OK) goto error;
 
+  data = OPERATION_CONTROL_REG_DATA;
+  if(rfidSpiTransmit(OPERATION_CONTROL_REG, &data, 1) != HAL_OK) goto error;
 
+  if(rfidSpiDrTransmit(STOP_ALL_ACTIVITIES, 1) != HAL_OK) goto error;
+  if(rfidSpiDrTransmit(RESET_RX_GAIN, 1) != HAL_OK) goto error;
+  delay(10);
+
+  cliPrintf("RFID INIT SUCCESS\n");
   return;
 
 error:
   RFID_tbl.isinit = false;
+  cliPrintf("RFID INIT FAILED\n");
   return;
 }
 
+void rfidReset(void)
+{
+
+}
+
+bool init_timer = false;
+uint8_t timer = 0;
 void rfidMain(void)
 {
+  if(BSP_LED_GetState(LED_BLUE) == true)
+  {
+    if(init_timer == false)
+    {
+      init_timer = true;
+      timer = millis() + 500;
+    }
+  }
 
+  if(init_timer && timer - millis() < 0)
+  {
+    BSP_LED_Off(LED_BLUE);
+  }
 }
 
 
 
-void HAL_GPIO_EXTI_Rising_Callback(uint16_t GPIO_Pin)
-{
-	if(GPIO_Pin == GPIO_PIN_0)
-	{
-		BSP_LED_On(LED_BLUE);
-	}
-}
+//void HAL_GPIO_EXTI_Rising_Callback(uint16_t GPIO_Pin)
+//{
+//	if(GPIO_Pin == GPIO_PIN_0)
+//	{
+//		BSP_LED_On(LED_BLUE);
+//	}
+//}
 
 void HAL_GPIO_EXTI_Falling_Callback(uint16_t GPIO_Pin)
 {
--- 2.firmware/Comp/RFID.h
+++ 2.firmware/Comp/RFID.h
@@ -29,7 +29,42 @@ typedef enum {
   IC_IDENTITY             = 0x3F,
 }RFID_REG;
 
-#define ST25R3916B_IC_IDENTITY  0x31
+typedef enum{
+  SET_DEFAULT =                 0xC0,
+  STOP_ALL_ACTIVITIES =         0xC2,
+  TRANSMIT_WITH_CRC =           0xC4,
+  TRANSMIT_WITHOUT_CRC =        0xC5,
+  TRANSMIT_REQA =               0xC6,
+  TRANSMIT_WUPA =               0xC7,
+  NFC_INITIAL_FIELD_ON =        0xC8,
+  NFC_RESPONSE_FIELD_ON =       0xC9,
+  GO_TO_SENSE =                 0xCD,
+  GO_TO_SLEEP =                 0xCE,
+  MASK_RECEIVE_DATA =           0xD0,
+  UNMASK_RECEIVE_DATA =         0xD1,
+  CHANGE_AM_MODULATION_STATE =  0xD2,
+  MEASURE_AMPLITUDE =           0xD3,
+  RESET_RX_GAIN =               0xD5,
+  ADJUST_REGULATORS =           0xD6,
+  CALIBRATE_DRIVER_TIMING =     0xD8,
+  MEASURE_PHASE =               0xD9,
+  CLEAR_RSSI =                  0xDA,
+  CLEAR_FIFO =                  0xDB,
+  ENTER_TRANSPARENT_MODE =      0xDC,
+  MEASURE_POWER_SUPPLY =        0xDF,
+  START_GENERAL_PURPOSE_TIMER = 0xE0,
+  START_WAKEUP_TIMER =          0xE1,
+  START_MASK_RECEIVE_TIMER =    0xE2,
+  START_NO_RESPONSE_TIMER =     0xE3,
+  START_PP_TIMER =              0xE4,
+  STOP_NO_RESPONSE_TIMER =      0xE8,
+
+}RFID_DM;
+
+#define ST25R3916B_IC_IDENTITY          0x31
+#define IO_CONFIGURATION_REG_01_DATA    0x0C
+#define IO_CONFIGURATION_REG_02_DATA    0x84
+#define OPERATION_CONTROL_REG_DATA      0xC0
 
 void rfidInit(void);
 void rfidMain(void);
